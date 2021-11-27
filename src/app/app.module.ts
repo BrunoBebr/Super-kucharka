8fa2a5c20a11df11403bb98fd96a9d604d9b4720
Bruno B <letaxcz@gmail.com>
2021-11-27 21:40:33 +0100

    Stepper v addRecepty + button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 import { MainPageComponent } from './main-page/main-page/main-page.component';
 import { ReceptCardComponent } from './main-page/recept-card/recept-card.component';
@@ -31,7 +35,12 @@ import { ReceptAddComponent } from './main-page/recept-add/recept-add.component'
     MatButtonModule,
     MatCardModule,
     MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule
 
   ],
   providers: [],
--- src/app/main-page/recept-add/recept-add.component.ts
+++ src/app/main-page/recept-add/recept-add.component.ts
@@ -1,15 +1,54 @@
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-recept-add',
   templateUrl: './recept-add.component.html',
-  styleUrls: ['./recept-add.component.scss']
+  styleUrls: ['./recept-add.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {showError: true},
+    },
+  ],
 })
 export class ReceptAddComponent implements OnInit {
+  receptAddForm!: FormGroup;
+  //postup: FormArray | any[] | undefined;
 
-  constructor() { }
+  //currentStep = 0;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.receptAddForm = new FormGroup({
+      'zakladniUdaje': new FormGroup({
+        'nazev': new FormControl(null, Validators.required),
+        'autor': new FormControl(null, Validators.required),
+        'obrazek': new FormControl(null),
+        'poznamka': new FormControl(null, Validators.required)
+      }),
+     // postup: this._formBuilder.array([this.init()])    
+    })
+    //this.addItem();
   }
 
+ /* init(){
+    return this._formBuilder.group({
+        'nazev': new FormControl(null, Validators.required),
+        'cas': new FormControl(null, Validators.required),
+        'obrazek': new FormControl(null),
+        'postup': new FormControl(null, Validators.required)
+    })
+  }
+  
+  addItem(){
+    this.postup = this.receptAddForm.get('postup') as FormArray;
+    this.postup.push(this.init());
+  }*/
 }
+
+  
+
+
