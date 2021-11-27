417b4251d2721134aa10c36b2019bdf8ff816d25
Bruno B <letaxcz@gmail.com>
2021-11-27 11:23:02 +0100

    karta receptu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recepty } from './interface';
+import { ReceptyService } from './recepty.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Super-kucharka';
+
+  recepty: Recepty[] = [];
+  error = '';
+  success = '';
+        
+  constructor(private receptyService: ReceptyService) {
+  }
+        
+  ngOnInit() {
+    this.getRecepty();
+  }
+        
+  getRecepty(): void {
+    this.receptyService.getAll().subscribe(
+      (data: Recepty[]) => {
+        this.recepty = data;
+        this.success = 'successful retrieval of the list';
+      },
+      (err) => {
+        console.log(err);
+        this.error = err;
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+import { MainPageComponent } from './main-page/main-page.component';
+import { ReceptCardComponent } from './recept-card/recept-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ReceptCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
